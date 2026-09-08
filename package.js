5aca67c861713689e0e19e6035fcb565a19738ad
Tanvir Ahmed <submit_report@yahoo.com>
2026-09-08 18:42:46 +0600

    Initialize package.json for Tanvir143 API

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "tanvir143-api",
+  "version": "1.0.0",
+  "description": "Tanvir143 Image to Image API",
+  "scripts": {
+    "start": "netlify dev"
+  },
+  "devDependencies": {
+    "netlify-cli": "latest"
+  }
+}
